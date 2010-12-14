14637af822df8d74f44c9fc83a63278637d36819
Parth Mauria Saxena <a0131646@psplinux052.india.ti.com>
2010-12-14 18:25:47 +0530

    TI81XX: NAND: Change oob layout to synch with kernel
    This patch changes the oob layout in uboot for 1 bit hamming code
    and makes it in synch with the kernel
    
    Signed-off-by: Parth Mauria Saxena <a0131646@psplinux052.india.ti.com>

--- arch/arm/include/asm/arch-ti81xx/nand.h
+++ arch/arm/include/asm/arch-ti81xx/nand.h
@@ -60,6 +60,19 @@
 }
 #endif
 
+/* NAND device layout in synch with the kernel */
+#ifdef GPMC_NAND_ECC_LP_x16_LAYOUT
+#define GPMC_NAND_HW_ECC_LAYOUT_KERNEL {\
+	.eccbytes = 12,\
+	.eccpos = {\
+		   40, 41, 42, 43, 44, 45, 46, 47,\
+		   48, 49, 50, 51},\
+	.oobfree = {\
+		{.offset = 2,\
+		 .length = 38} } \
+}
+#endif
+
 /* Small Page x8 NAND device Layout */
 #ifdef GPMC_NAND_ECC_SP_x8_LAYOUT
 #define GPMC_NAND_HW_ECC_LAYOUT {\
