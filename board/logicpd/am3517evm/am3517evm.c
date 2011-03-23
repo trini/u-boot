b10b5a8f4d7b6b6be677b217694968030a229024
Abhilash Vadakkepat Koyamangalath <x0151633@psplinux051.india.ti.com>
2011-03-23 18:10:01 +0530

    [AM3517]: remove build warnings This patch removes the following build warnings:     am3517evm.c: In function 'misc_init_r':     am3517evm.c:80: warning: implicit declaration of function 	'omap_request_gpio'     am3517evm.c:81: warning: implicit declaration of function 	'omap_set_gpio_direction'     am3517evm.c:82: warning: implicit declaration of function 	'omap_set_gpio_dataout'     am3517evm.c: In function 'cpu_eth_init':     am3517evm.c:117: warning: implicit declaration of function 	'davinci_emac_initialize'

--- board/logicpd/am3517evm/am3517evm.c
+++ board/logicpd/am3517evm/am3517evm.c
@@ -27,11 +27,13 @@
 #include <asm/io.h>
 #include <asm/arch/mem.h>
 #include <asm/arch/mux.h>
+#include <asm/arch/gpio.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/mmc_host_def.h>
 #include <asm/arch/emac_defs.h>
 #include <asm/mach-types.h>
 #include <i2c.h>
+#include <netdev.h>
 #include "am3517evm.h"
 
 
