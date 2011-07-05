51cf1e7ce78ecdae8b6d10ab0ae6f411afeb6fe2
Sugumar Natarajan <sugumar@ti.com>
2011-07-05 09:24:35 +0530

    am335x: remove the compilation warnings
    Signed-off-by: Sugumar Natarajan <sugumar@ti.com>

--- board/ti/am335x/evm.c
+++ board/ti/am335x/evm.c
@@ -28,6 +28,7 @@
 #include <miiphy.h>
 #include <netdev.h>
 #include "common_def.h"
+#include <i2c.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -50,6 +51,8 @@ DECLARE_GLOBAL_DATA_PTR;
 #define I2C_IA_DB_EEPROM_ADDR	0x52
 #define I2C_IPP_DB_EEPROM_ADDR	0x52
 #define I2C_BB_EEPROM_ADDR	0x50
+
+extern void cpsw_eth_set_mac_addr(const u_int8_t *addr);
 
 void init_timer(void)
 {
@@ -254,6 +257,8 @@ int checkboard(void)
 		printf("board: IPP daughter card connected");
 	else
 		printf("board: No daughter card connected");
+
+	return 0;
 }
 
 #ifdef CONFIG_DRIVER_TI_CPSW
