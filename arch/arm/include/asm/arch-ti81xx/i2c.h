623d9c41c0ae34a63e4e8ce43b7e34e0a562c1aa
Philip, Avinash <avinashphilip@ti.com>
2011-08-23 12:24:57 +0530

    AM335x: I2C Multi byte address support for EEPROM
    This patch is to support 2 byte address write for EEPROM. Existing
    omap24xx_i2c.c does not support address length greater than one.
    
    1. CAT24C256 EEPROM requires writing two byte address for write/read
    operations.
    2. To support the EEPROM write/read support call the i2c_read/i2c_write
    API with alen 2.
    
    Also, this patch corrects the handling of error states in i2c functionalities.
    On detecting I2C bus busy, return from function with error status added.
    
    when we are at it,
    1. I2C printf are removed since these functionalities might be called
       before console sets up.
    2. Removed the unused functions & there by eliminating warning.
    
    Signed-off-by: Philip, Avinash <avinashphilip@ti.com>
    Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>

--- arch/arm/include/asm/arch-ti81xx/i2c.h
+++ arch/arm/include/asm/arch-ti81xx/i2c.h
@@ -90,6 +90,8 @@
 
 #define I2C_BUF_RDMA_EN		(1 << 15) /* Receive DMA channel enable */
 #define I2C_BUF_XDMA_EN		(1 << 7)  /* Transmit DMA channel enable */
+#define I2C_TXFIFO_CLEAR        (1 << 6)  /* TX FIFO clear */
+#define I2C_RXFIFO_CLEAR        (1 << 14) /* RX FIFO Clear */
 
 /* I2C Configuration Register (I2C_CON): */
 
