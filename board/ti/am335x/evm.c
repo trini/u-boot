ba47146f06e2753b5276f6cace9948766b4c2db0
Hebbar, Gururaja <gururaja.hebbar@ti.com>
2011-09-06 17:11:50 +0530

    AM335X: Removed unused ddr config file
    Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>

--- board/ti/am335x/evm.c
+++ board/ti/am335x/evm.c
@@ -364,9 +364,6 @@ static void config_am335x_mddr(void)
 /*  void DDR2_EMIF_Config(void); */
 static void config_am335x_ddr2(void)
 {
-/*#if 1
-	DDR2_EMIF_Config();
-#else*/
 	int data_macro_0 = 0;
 	int data_macro_1 = 1;
 
@@ -392,7 +389,6 @@ static void config_am335x_ddr2(void)
 	__raw_writel(__raw_readl(DDR_CKE_CTRL) | 0x00000001, DDR_CKE_CTRL);
 
 	config_emif_ddr2();
-/* #endif */
 }
 
 static void config_am335x_ddr(void)
