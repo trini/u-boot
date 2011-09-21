479f90c6097c9e9d472ca9461afbdf965f0ef379
Tom Rini <trini@ti.com>
2011-09-21 13:30:58 -0700

    am335x: Fix various warnings
    Signed-off-by: Tom Rini <trini@ti.com>

--- board/ti/am335x/pll.c
+++ board/ti/am335x/pll.c
@@ -134,7 +134,7 @@ static void per_clocks_enable(void)
 
 static void mpu_pll_config(void)
 {
-	u32 clkmode, clksel, div_m2, idlest_dpll;
+	u32 clkmode, clksel, div_m2;
 
 	clkmode = __raw_readl(CM_CLKMODE_DPLL_MPU);
 	clksel = __raw_readl(CM_CLKSEL_DPLL_MPU);
@@ -161,7 +161,7 @@ static void mpu_pll_config(void)
 
 static void core_pll_config(void)
 {
-	u32 clkmode, clksel, div_m4, div_m5, div_m6, idlest_dpll;
+	u32 clkmode, clksel, div_m4, div_m5, div_m6;
 
 	clkmode = __raw_readl(CM_CLKMODE_DPLL_CORE);
 	clksel = __raw_readl(CM_CLKSEL_DPLL_CORE);
@@ -199,7 +199,7 @@ static void core_pll_config(void)
 
 static void per_pll_config(void)
 {
-	u32 clkmode, clksel, div_m2, idlest_dpll;
+	u32 clkmode, clksel, div_m2;
 
 	clkmode = __raw_readl(CM_CLKMODE_DPLL_PER);
 	clksel = __raw_readl(CM_CLKSEL_DPLL_PER);
@@ -226,7 +226,7 @@ static void per_pll_config(void)
 
 static void ddr_pll_config(void)
 {
-	u32 clkmode, clksel, div_m2, idlest_dpll;
+	u32 clkmode, clksel, div_m2;
 
 	clkmode = __raw_readl(CM_CLKMODE_DPLL_DDR);
 	clksel = __raw_readl(CM_CLKSEL_DPLL_DDR);
