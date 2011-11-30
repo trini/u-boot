cd603de396b41128a3c8a2245c99b6fe6a67b284
Tom Rini <trini@ti.com>
2011-11-30 16:16:21 -0700

    beagleboard: Make rev C go to 1GHz
    This is TI Sitara SDK specific (run at maximum speed even if it lowers
    the lifespan of the part).
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- board/ti/beagle/beagle.c
+++ board/ti/beagle/beagle.c
@@ -314,6 +314,9 @@ int misc_init_r(void)
 					TWL4030_PM_RECEIVER_VAUX2_VSEL_18,
 					TWL4030_PM_RECEIVER_VAUX2_DEV_GRP,
 					TWL4030_PM_RECEIVER_DEV_GRP_P1);
+		/* 1GHz support */
+		twl4030_power_mpu_init();
+		set_mpu_clk(1000);
 		break;
 	default:
 		printf("Beagle unknown 0x%02x\n", get_board_revision());
